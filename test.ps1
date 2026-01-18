7179ca873f63b1680e5ff22a5eb18d8c88d8fb62
end-c <528189644@qq.com>
2026-01-18 13:47:05 +0800

    test

---
+++ test.ps1
@@ -0,0 +1,3 @@
+Write-Host "DEBUG: proc is null? $($null -eq $proc)" -ForegroundColor Yellow
+Write-Host "DEBUG: proc type = $($proc.GetType().FullName)" -ForegroundColor Yellow
+Write-Host "DEBUG: proc id = $($proc.Id)" -ForegroundColor Yellow
