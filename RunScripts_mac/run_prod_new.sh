a082f98f74e61c0c4dd025b976d9165a409b9e47
end-c <528189644@qq.com>
2026-02-01 09:32:55 +0800

    run_prod_new.sh

--- RunScripts_mac/run_prod_new.sh
+++ RunScripts_mac/run_prod_new.sh
@@ -149,7 +149,7 @@ for accountId in "${ACCOUNT_SET[@]}"; do
       "$PROC_SCRIPT" \
       --private_key "$private_key" \
       --account_id "$accountId" \
-      2>&1 | tee -a "$LOG" &
+      >> "$LOG" 2>&1 &
 
   PID=$!
 
